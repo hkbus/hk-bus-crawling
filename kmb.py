2e55b455feb6f76caefbd962455bae41994b04e4
Law Wai Chun <chunlaw@rocketmail.com>
2022-04-29 11:09:08 +0800

    Handle non-exist stop

--- kmb.py
+++ kmb.py
@@ -2,11 +2,28 @@ import requests
 import json
 from os import path
 import copy
+import sys
 
 def getRouteStop(co = 'kmb'):
     # define output name
     ROUTE_LIST = 'routeList.'+co+'.json'
     STOP_LIST = 'stopList.'+co+'.json'
+
+    stopList = {}
+    if path.isfile(STOP_LIST):
+        with open(STOP_LIST) as f:
+            stopList = json.load(f)
+    else:
+        # load stops
+        r = requests.get('https://data.etabus.gov.hk/v1/transport/'+co+'/stop')
+        _stopList = r.json()['data']
+        for stop in _stopList:
+            stopList[stop['stop']] = stop
+
+    def isStopExist( stopId ):
+        if stopId not in stopList:
+            print ("Not exist stop: ", stopId, file=sys.stderr)
+        return stopId in stopList
 
     # load route list and stop list if exist
     routeList = {}
@@ -35,22 +52,12 @@ def getRouteStop(co = 'kmb'):
         # flatten the route stops back to array
         for routeKey in routeList.keys():
             stops = [routeList[routeKey]['stops'][seq] for seq in sorted(routeList[routeKey]['stops'].keys())]
+            # filter non-exist stops
+            stops = list(filter(isStopExist, stops))
             routeList[routeKey]['stops'] = stops
 
         # flatten the routeList back to array
         routeList = [routeList[routeKey] for routeKey in routeList.keys() if not routeKey.startswith('K')]
-
-
-    stopList = {}
-    if path.isfile(STOP_LIST):
-        with open(STOP_LIST) as f:
-            stopList = json.load(f)
-    else:
-        # load stops
-        r = requests.get('https://data.etabus.gov.hk/v1/transport/'+co+'/stop')
-        _stopList = r.json()['data']
-        for stop in _stopList:
-            stopList[stop['stop']] = stop
    
     with open(ROUTE_LIST, 'w') as f:
         f.write(json.dumps(routeList, ensure_ascii=False))

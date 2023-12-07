2ebc17630c211163a42dd35fc57ffeb70dda90eb
Raymond Tau <raymondtau@gmail.com>
2023-12-08 03:34:59 +0800

    feat: Use httpx async for nlb

--- crawling/nlb.py
+++ crawling/nlb.py
@@ -1,33 +1,38 @@
-import requests
-import json
-from os import path
 import asyncio
-import time
+import json
+import logging
+from os import path
 
-def emitRequest(url):
+import httpx
+
+logger=logging.getLogger(__name__)
+
+async def emitRequest(url:str, client:httpx.AsyncClient):
   # retry if "Too many request (429)"
   while True:
-    r = requests.get(url)
+    r = await client.get(url)
     if r.status_code == 200:
       return r
     elif r.status_code == 429 or r.status_code == 502:
-      time.sleep(1)
+      await asyncio.sleep(1)
     else:
+      r.raise_for_status()
       raise Exception(r.status_code, url)
 
-def getRouteStop(co):
+async def getRouteStop(co):
     # define output name
     ROUTE_LIST = 'routeList.'+co+'.json'
     STOP_LIST = 'stopList.'+co+'.json'
 
+    a_client = httpx.AsyncClient()
     # load route list and stop list if exist
-    routeList = {}
+    routeList = []
     if path.isfile(ROUTE_LIST):
+        logger.warning(f"{ROUTE_LIST} already exist, skipping...")
         return
     else:
         # load routes
-        r = emitRequest('https://rt.data.gov.hk/v2/transport/nlb/route.php?action=list')
-        routeList = []
+        r = await emitRequest('https://rt.data.gov.hk/v2/transport/nlb/route.php?action=list', a_client)
         for route in r.json()['routes']:
             routeList.append({
                 "id": route['routeId'],
@@ -41,55 +46,55 @@ def getRouteStop(co):
                 "stops": [],
                 "co": ["nlb"]
             })
+        logger.info("Digested route list")
 
-    _stops = []
     stopList = {}
     if path.isfile(STOP_LIST):
         with open(STOP_LIST) as f:
             stopList = json.load(f)
    
-    def getRouteStop(routeId):
-        r = emitRequest('https://rt.data.gov.hk/v2/transport/nlb/stop.php?action=list&routeId='+routeId)
+    async def getRouteStop(routeId):
+        r = await emitRequest('https://rt.data.gov.hk/v2/transport/nlb/stop.php?action=list&routeId='+routeId, a_client)
         try:
             return r.json()['stops']
         except Exception as err:
             print(r)
             raise err
 
+    async def addRouteStop(route):
+        stops = await getRouteStop(route['id'])
+        stopIds = []
+        fares = []
+        faresHoliday = []
+        for stop in stops:
+            if stop['stopId'] not in stopList:
+                stopList[stop['stopId']] = {
+                    'stop': stop['stopId'],
+                    'name_en': stop['stopName_e'],
+                    'name_tc': stop['stopName_c'],
+                    'lat': stop['latitude'],
+                    'long': stop['longitude']
+                }
+            stopIds.append(stop['stopId'])
+            fares.append(stop['fare'])
+            faresHoliday.append(stop['fareHoliday'])
+        route['stops'] = stopIds
+        route['fares'] = fares[0:-1]
+        route['faresHoliday'] = faresHoliday[0:-1]
+
     async def getRouteStopList ():
-        loop = asyncio.get_event_loop()
-        futures = [loop.run_in_executor(None, getRouteStop, route['id']) for route in routeList]
-        ret = []
-        for future in futures:
-            ret.append(await future)
-        for route, stops in zip(routeList, ret):
-            stopIds = []
-            fares = []
-            faresHoliday = []
-            for stop in stops:
-                if stop['stopId'] not in stopList:
-                    stopList[stop['stopId']] = {
-                        'stop': stop['stopId'],
-                        'name_en': stop['stopName_e'],
-                        'name_tc': stop['stopName_c'],
-                        'lat': stop['latitude'],
-                        'long': stop['longitude']
-                    }
-                stopIds.append(stop['stopId'])
-                fares.append(stop['fare'])
-                faresHoliday.append(stop['fareHoliday'])
-            route['stops'] = stopIds
-            route['fares'] = fares[0:-1]
-            route['faresHoliday'] = faresHoliday[0:-1]
+        await asyncio.gather(*[addRouteStop(r) for r in routeList])
+        logger.info("Digested stop list")
         return routeList
 
+    await getRouteStopList()
 
-    loop = asyncio.get_event_loop()
-    routeList = loop.run_until_complete(getRouteStopList())
+    with open(ROUTE_LIST, 'w') as rf, open(STOP_LIST, 'w') as sf:
+        json.dump(routeList, rf, ensure_ascii=False)
+        json.dump(stopList, sf, ensure_ascii=False)
+    logger.info("Dumped lists")
 
-    with open(ROUTE_LIST, 'w') as f:
-        f.write(json.dumps(routeList, ensure_ascii=False))
-    with open(STOP_LIST, 'w') as f:
-        f.write(json.dumps(stopList, ensure_ascii=False))
-
-getRouteStop('nlb')
+if __name__=='__main__':
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger('httpx').setLevel(logging.WARNING)
+    asyncio.run(getRouteStop('nlb'))

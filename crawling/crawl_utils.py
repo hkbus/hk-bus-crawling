8bb3f2e5285667af7196f7eaa117b496237b2480
Raymond Tau <raymondtau@gmail.com>
2024-02-19 14:14:52 +0800

    fix: Improve crawl exception handling

--- crawling/crawl_utils.py
+++ crawling/crawl_utils.py
@@ -5,6 +5,8 @@ import logging
 logger=logging.getLogger(__name__)
 
 async def emitRequest(url:str,client: httpx.AsyncClient):
+  RETRY_TIMEOUT_MAX=60
+  retry_timeout=1
   # retry if "Too many request (429)"
   while True:
     try:
@@ -12,11 +14,13 @@ async def emitRequest(url:str,client: httpx.AsyncClient):
       if r.status_code == 200:
         return r
       elif r.status_code == 429 or r.status_code == 502:
-        logger.warning(f"status_code={r.status_code}, wait and retry")
-        await asyncio.sleep(1)
+        logger.warning(f"status_code={r.status_code}, wait {retry_timeout} and retry. URL={url}")
+        await asyncio.sleep(retry_timeout)
+        retry_timeout = min (retry_timeout * 2, RETRY_TIMEOUT_MAX)
       else:
         r.raise_for_status()
         raise Exception(r.status_code, url)
     except (httpx.PoolTimeout, httpx.ReadTimeout, httpx.ReadError) as e:
-      logger.warning(f"Exception {repr(e)} occurred, retrying")
-      await asyncio.sleep(1)
+      logger.warning(f"Exception {repr(e)} occurred, wait {retry_timeout} and retry. URL={url}")
+      await asyncio.sleep(retry_timeout)
+      retry_timeout = min (retry_timeout * 2, RETRY_TIMEOUT_MAX)

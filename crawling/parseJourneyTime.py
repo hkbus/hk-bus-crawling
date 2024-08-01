9c03ad38dab82a606d9717284800ebecf3beb391
Raymond Tau <raymondtau@gmail.com>
2024-08-02 00:34:18 +0800

    feat: Switch to use async emitRequest for parseJourneyTime

--- crawling/parseJourneyTime.py
+++ crawling/parseJourneyTime.py
@@ -1,24 +1,36 @@
+import asyncio
+import logging
 import xml.etree.ElementTree as ET
-import requests
+import httpx
 from os import path
-
-if not path.isfile('ROUTE_BUS.xml'):
-  r = requests.get('https://static.data.gov.hk/td/routes-fares-xml/ROUTE_BUS.xml')
-  r.encoding = 'utf-8'
-  with open('ROUTE_BUS.xml', 'w', encoding='UTF-8') as f:
-    f.write(r.text)
-
-routeTimeList = {}
-tree = ET.parse('ROUTE_BUS.xml')
-root = tree.getroot()
-for route in root.iter('ROUTE'):
-  if route.find('ROUTE_TYPE').text == '1':
-    routeTimeList[route.find('ROUTE_ID').text] = {
-      'co': route.find('COMPANY_CODE').text.replace('LWB', 'KMB').lower().split('+'),
-      'route': route.find('ROUTE_NAMEC').text,
-      'journeyTime': route.find('JOURNEY_TIME').text,
-    }
-
 import json
-with open('routeTime.json', 'w', encoding='UTF-8') as f:
-  f.write(json.dumps(routeTimeList, ensure_ascii=False))
+
+from crawl_utils import emitRequest
+
+async def parseJourneyTime():
+  a_client = httpx.AsyncClient(timeout=httpx.Timeout(30.0, pool=None))
+  if not path.isfile('ROUTE_BUS.xml'):
+    r = await emitRequest('https://static.data.gov.hk/td/routes-fares-xml/ROUTE_BUS.xml', a_client)
+    r.encoding = 'utf-8'
+    with open('ROUTE_BUS.xml', 'w', encoding='UTF-8') as f:
+      f.write(r.text)
+
+  routeTimeList = {}
+  tree = ET.parse('ROUTE_BUS.xml')
+  root = tree.getroot()
+  for route in root.iter('ROUTE'):
+    if route.find('ROUTE_TYPE').text == '1':
+      routeTimeList[route.find('ROUTE_ID').text] = {
+        'co': route.find('COMPANY_CODE').text.replace('LWB', 'KMB').lower().split('+'),
+        'route': route.find('ROUTE_NAMEC').text,
+        'journeyTime': route.find('JOURNEY_TIME').text,
+      }
+
+  with open('routeTime.json', 'w', encoding='UTF-8') as f:
+    f.write(json.dumps(routeTimeList, ensure_ascii=False))
+
+if __name__=='__main__':
+  logging.basicConfig(level=logging.INFO)
+  logging.getLogger('httpx').setLevel(logging.WARNING)
+  logger = logging.getLogger(__name__)
+  asyncio.run(parseJourneyTime())

9cbaed0d1aedc43a72d44620f14fa3252d3bd6bd
kaon <kaon@users.noreply.github.com>
2025-01-22 17:29:37 +0000

    Formatted Code!

--- crawling/parseHoliday.py
+++ crawling/parseHoliday.py
@@ -7,18 +7,19 @@ import asyncio
 
 logger = logging.getLogger(__name__)
 
+
 async def main():
   if not path.isfile('holiday.json'):
     async with httpx.AsyncClient() as a_client:
       r = await a_client.get('https://www.1823.gov.hk/common/ical/tc.json')
       data = r.json()
     with open('holiday.json', 'w') as f:
-      json.dump([holiday['dtstart'][0]
-                for holiday in data['vcalendar'][0]['vevent'] if int(holiday['dtstart'][0][:4]) >= datetime.now().year], f)
+      json.dump([holiday['dtstart'][0] for holiday in data['vcalendar'][0][
+                'vevent'] if int(holiday['dtstart'][0][:4]) >= datetime.now().year], f)
     logger.info('Created holiday.json')
   else:
     logger.info('holiday.json already exist, download skipped')
 
-if __name__=='__main__':
+if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO)
   asyncio.run(main())
